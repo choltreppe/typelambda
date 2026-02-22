f231cf2bc176492e04c780de306ba2053cf7507c
Joel Lienhard <joel.lienhard@stud.uni-hannover.de>
2026-02-22 22:30:05 +0100

    add info box

--- src/main.nim
+++ src/main.nim
@@ -23,13 +23,23 @@ setRenderer do(route: RouterData) -> VNode:
 
   buildHtml(tdiv):
     form:
-      input(`type` = "text", value = exprStr)
-      input(`type` = "submit", value = "compute")
+      input(`type` = "text", value = exprStr, placeholder = "enter simply typed lambda expression ..")
+      input(`type` = "submit", value = "deduce")
       proc onsubmit(e: Event, n: VNode) =
         e.preventDefault()
         window.location.hash = $n.dom[0].InputElement.value
 
     if deduction.isOk():
       render(deduction.vResultPrivate)
+    
     elif (let e = deduction.error; e.msg != ""):
       tdiv(id = "error"): text e.msg
+    
+    else:
+      tdiv(id = "info"):
+        text "some example expressions, showing whats supported:"
+        ul:
+          li: text r"λx: Bool → Nat. x true"
+          li: text r"\x: Bool -> Nat. x true"
+          li: text r"(\x: Bool. if x then 0 else 42) false"
+          li: text r"(\f: Int -> Int. \x: Int. (f x) + 2) (\x: Int. x * 2) 4"
